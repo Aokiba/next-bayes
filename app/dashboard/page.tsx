47ecb77f71e6c843d917bbbc3ce6e31a259e08e7
Sahara <60813677+lwx-fly@users.noreply.github.com>
2024-04-29 13:12:58 +0800

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -217,18 +217,5 @@ function UsersIcon(props) {
 // // This is the root layout component for your Next.js app.
 // // Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
 
-// import { Rethink_Sans } from 'next/font/google'
-// import { Fira_Mono } from 'next/font/google'
-// import './styles.css'
 
-// const rethink_sans = Rethink_Sans({
-//   subsets: ['latin'],
-//   display: 'swap',
-//   variable: '--font-rethink_sans',
-// })
-// const fira_mono = Fira_Mono({
-//   subsets: ['latin'],
-//   display: 'swap',
-//   variable: '--font-fira_mono',
-// })
 
